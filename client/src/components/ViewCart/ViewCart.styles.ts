faea0431b5d08dde4354d40d3e97c73f6003861b
Quan MQ <ngoleminhquan1994@gmail.com>
2021-12-09 09:15:08 +0700

    Finished View Cart

---
+++ client/src/components/ViewCart/ViewCart.styles.ts
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import { makeStyles, createStyles, Theme } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      paddingTop: "6rem",
+    },
+    crumbs: {
+      textTransform: "capitalize",
+      cursor: "pointer",
+      transition: "color 0.15s linear",
+      "&:hover": {
+        color: "#717FE0",
+      },
+    },
+    button: {
+      fontWeight: "bold",
+      borderRadius: "20px",
+      backgroundColor: "#222",
+      color: "#fff",
+      "&:hover": {
+        backgroundColor: "#717FE0",
+      },
+    },
+    table: {
+      "& thead th": {
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontSize: "1.25rem",
+        fontFamily:
+          "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+      },
+      "& td": {
+        fontSize: "1.15rem",
+      },
+      "& #error": {
+        marginTop: "10px",
+      },
+      "& .price, & .total": {
+        [theme.breakpoints.down(900)]: {
+          display: "none",
+        },
+      },
+      "& [type='number']": {
+        width: "100px",
+        [theme.breakpoints.down(900)]: {
+          width: "50px",
+        },
+      },
+    },
+  })
+);
+
+export const Wrapper = styled.div`
+  margin-left: 20px;
+  max-width: 80px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover::before {
+    content: "Remove";
+    font-weight: bold;
+    font-size: 1rem;
+    position: absolute;
+    color: #fff;
+    z-index: 10;
+  }
+
+  &:hover::after {
+    content: "";
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.4);
+    height: 100%;
+    width: 100%;
+  }
+
+  img {
+    width: 100%;
+    object-fit: contain;
+
+    @media (max-width: 900px) {
+      width: 70px;
+    }
+  }
+`;
---
+++ client/src/components/ViewCart/ViewCart.tsx
@@ -0,0 +1,184 @@
+import { Fragment, useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
+import { Container } from "@material-ui/core";
+import {
+  Breadcrumbs,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableFooter,
+  ButtonGroup,
+  Button,
+  IconButton,
+  TextField,
+} from "@material-ui/core";
+import { NavigateNext, Remove, Add } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import { useStyles, Wrapper } from "./ViewCart.styles";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+// *Types
+import { CartItemType } from "../../App";
+
+function Checkout() {
+  const { container, crumbs, button, table } = useStyles();
+  const {
+    state: { cartItems },
+    dispatch,
+  } = useContext(GlobalContext);
+
+  // *Actions
+  const handleAddToCart = (clickedItem: CartItemType, quantity: number) => {
+    if (quantity >= 0) {
+      clickedItem.amount = quantity;
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: clickedItem,
+      });
+    }
+  };
+
+  function addToCart(clickedItem: CartItemType) {
+    dispatch({
+      type: "ADD_SINGLE_ITEM_TO_CART",
+      payload: clickedItem,
+    });
+  }
+
+  function removeFromCart(clickedItem: CartItemType) {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: clickedItem,
+    });
+  }
+
+  function removeAllFromCart(clickedItem: CartItemType) {
+    dispatch({
+      type: "REMOVE_ALL_FROM_CART",
+      payload: clickedItem,
+    });
+  }
+
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) => acc + item.amount * item.price, 0);
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Container className={container}>
+        <Breadcrumbs
+          separator={<NavigateNext fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Typography color="textPrimary" className={crumbs}>
+            Home
+          </Typography>
+          <Typography color="textPrimary">Shopping Cart</Typography>
+        </Breadcrumbs>
+        <TableContainer>
+          <Table aria-label="simple table" className={table}>
+            <TableHead>
+              <TableRow>
+                <TableCell colSpan={2}>Product</TableCell>
+                <TableCell align="center" className="price">
+                  Price
+                </TableCell>
+                <TableCell align="center">Quantity</TableCell>
+                <TableCell align="center" className="total">
+                  Total
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {cartItems.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell component="th" scope="row">
+                    <Wrapper onClick={() => removeAllFromCart(item)}>
+                      <img src={item.image} alt={item.title} />
+                    </Wrapper>
+                  </TableCell>
+                  <TableCell>{item.title}</TableCell>
+                  <TableCell align="center" className="price">
+                    ${item.price}
+                  </TableCell>
+                  <TableCell align="center">
+                    <ButtonGroup
+                      size="small"
+                      variant="contained"
+                      color="primary"
+                      disableRipple
+                    >
+                      <IconButton
+                        onClick={() => {
+                          removeFromCart(item);
+                        }}
+                      >
+                        <Remove />
+                      </IconButton>
+                      <IconButton>
+                        <TextField
+                          type="number"
+                          value={item.amount}
+                          variant="outlined"
+                          size="small"
+                          onChange={({ target }) => {
+                            handleAddToCart(item, +target.value);
+                          }}
+                        />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => {
+                          addToCart(item);
+                        }}
+                      >
+                        <Add />
+                      </IconButton>
+                    </ButtonGroup>
+                    {item.amount === 0 && (
+                      <Typography color="error" id="error">
+                        Error: Number of items cannot be smaller than 1
+                      </Typography>
+                    )}
+                  </TableCell>
+                  <TableCell align="center" className="total">
+                    ${(item.amount * item.price).toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={2}>
+                  <Typography variant="h5" color="textPrimary">
+                    Total: ${calculateTotal(cartItems).toFixed(2)}
+                  </Typography>
+                </TableCell>
+                <TableCell colSpan={2}>
+                  <Button
+                    className={button}
+                    component={Link}
+                    to="/checkout"
+                    size="large"
+                    disableElevation
+                    fullWidth
+                    variant="contained"
+                  >
+                    Check Out
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </Container>
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default Checkout;
